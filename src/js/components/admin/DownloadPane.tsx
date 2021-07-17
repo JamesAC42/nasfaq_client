8f8a36f246ef5e58cae3689261ef886f5cca246b
James Crovo <jamescrovo2019@lschs.org>
2021-07-16 23:19:39 -0400

    Add tab on admin page to download the stat file

---
+++ src/js/components/admin/DownloadPane.tsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Button from '../Button';
+
+class DownloadPane extends Component {
+    render() {
+        return(
+            <div className="download-pane flex-col flex-center">
+                <div className="control-header">
+                    Download
+                </div>
+                <div className="control-description">
+                    Download the current data file. Only accessible to administrators. 
+                </div>
+                <a href="/api/getDataFile" target="__blank">Download</a>
+                <p>
+                    Note: This reads the file directly from the server, do it sparingly.
+                </p>
+            </div>
+        )
+    }
+}
+
+export default DownloadPane;
