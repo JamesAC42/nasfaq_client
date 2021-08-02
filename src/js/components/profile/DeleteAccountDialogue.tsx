93efad099d127a0e158fd352e6b767319c05811a
James Crovo <jamescrovo2019@lschs.org>
2021-08-02 00:16:53 -0400

    Require password when deleting account

--- src/js/components/profile/DeleteAccountDialogue.tsx
+++ src/js/components/profile/DeleteAccountDialogue.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '../Button';
+import { PasswordItem } from '../InputItem';
 
 interface DeleteAccountDialogueProps {
     visible: boolean,
@@ -7,11 +8,39 @@ interface DeleteAccountDialogueProps {
     toggle: () => void
 }
 
+class DeleteAccountDialogueState {
+    password:string;
+    error:string;
+    constructor() {
+        this.password = "";
+        this.error = "";
+    }
+}
+type formEvent = React.ChangeEvent<HTMLInputElement>;
+
 class DeleteAccountDialogue extends Component<DeleteAccountDialogueProps> {
     
+    state:DeleteAccountDialogueState;
+    constructor(props:DeleteAccountDialogueProps) {
+        super(props);
+        this.state = new DeleteAccountDialogueState();
+    }
+    updatePassword = (e:formEvent) => {
+        this.setState({
+            ...this.state,
+            password:e.target.value
+        });
+    }
     deleteAccount() {
+        if(this.state.password === "") {
+            this.setState({error:"You must enter your password"});
+            return;
+        }
         fetch('/api/deleteOwnAccount/', {
-            method: 'GET',
+            method: 'POST',
+            body: JSON.stringify({
+                password:this.state.password
+            }),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -20,6 +49,11 @@ class DeleteAccountDialogue extends Component<DeleteAccountDialogueProps> {
         .then(data => {
             if(data.success) {
                 this.props.deleted();
+            } else {
+                this.setState({
+                    password:"",
+                    error:data.message
+                })
             }
         })
         .catch(error => {
@@ -33,13 +67,19 @@ class DeleteAccountDialogue extends Component<DeleteAccountDialogueProps> {
             <div className="dialogue center-child">
                 <div className="dialogue-container delete-dialogue">
                     <div className="delete-message">
-                        Are you sure you want to delete your account?
+                        Are you sure you want to delete your account? You can't undo this!
                     </div>
+                    <div className="input-label">Password</div>
+                        <PasswordItem
+                            value={this.state.password} 
+                            maxLength={200}
+                            name={"password"}
+                            updateValue={this.updatePassword} />
                     <div className="delete-options">
                         <Button 
                             className="red inverse"
                             onClick={() => this.deleteAccount()}>
-                                Yes
+                                Delete
                         </Button>
                         <Button 
                             className="green inverse"
@@ -47,6 +87,7 @@ class DeleteAccountDialogue extends Component<DeleteAccountDialogueProps> {
                                 Cancel
                         </Button>
                     </div>
+                    <div className="delete-error">{this.state.error}</div>
                 </div>
             </div>
         )
