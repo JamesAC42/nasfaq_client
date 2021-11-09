850a6bcc62ec7cc4efc497b399202a0bb13c8e43
James Crovo <jamescrovo2019@lschs.org>
2021-11-08 19:21:19 -0500

    Add filters to auction page to make navigation easier

--- src/js/components/auctions/Auctions.tsx
+++ src/js/components/auctions/Auctions.tsx
@@ -7,7 +7,7 @@ import DropdownInput from '../DropdownInput';
 import AuctionPreview from './AuctionPreview';
 import AuctionHistory from './AuctionHistory';
 import CreateAuction from './CreateAuction';
-import { IAuctionItem } from './IAuction';
+import { IAuctionHistory, IAuctionItem } from './IAuction';
 import { Redirect } from 'react-router';
 import BuyLicense from './BuyLicense';
 
@@ -16,16 +16,50 @@ import AuctionInfo from './AuctionInfo';
 import {
     AiOutlineInfoCircle
 } from 'react-icons/ai';
+import { auctionsActions } from '../../actions/actions';
+import storageAvailable from '../../checkStorage';
 
 const mapStateToProps = (state:any) => ({
     userinfo:state.userinfo,
     session:state.session,
-    auctions:state.auctions
-})
+    auctions:state.auctions,
+    itemcatalogue:state.itemcatalogue
+});
+
+const mapDispatchToProps = {
+    setAuctionSubscriptions: auctionsActions.setAuctionSubscriptions
+}
 
 enum AuctionView {
     LIVE,
     PAST
+}
+
+enum AuctionSort {
+    Price,
+    ExpirationTime,
+    Item,
+    Seller,
+    Bidder
+}
+
+class AuctionFilters {
+    myBids:boolean;
+    mySales:boolean;
+    watching:boolean;
+    item:string|null;
+    hasBid:boolean;
+    noBidders:boolean;
+    sortMethod:AuctionSort;
+    constructor() {
+        this.myBids = false;
+        this.mySales = false;
+        this.watching = false; 
+        this.item = null;
+        this.hasBid = false;
+        this.noBidders = false;
+        this.sortMethod = AuctionSort.ExpirationTime;
+    }
 }
 
 class AuctionsState {
@@ -33,25 +67,36 @@ class AuctionsState {
     showCreateWindow: boolean;
     showBuyLicenseWindow: boolean;
     showInfoWindow: boolean;
+    showFilters: boolean;
+    showItemSelect: boolean;
+    filters:AuctionFilters;
     constructor() {
         this.showInfoWindow = false;
         this.activeView = AuctionView.LIVE;
         this.showCreateWindow = false;
         this.showBuyLicenseWindow = false;
+        this.showFilters = false;
+        this.showItemSelect = false;
+        this.filters = new AuctionFilters();
     } 
 }
 
 interface AuctionsProps {
     userinfo: {
         items:any,
-        loaded:boolean
+        loaded:boolean,
+        username:string,
+        id:string
     },
     session: {
         loggedin: boolean
     },
     auctions: {
-        activeAuctions:Array<IAuctionItem>
-    }
+        activeAuctions:Array<IAuctionItem>,
+        subscriptions:Array<string>
+    },
+    itemcatalogue: any,
+    setAuctionSubscriptions: (subscriptions:any) => {}
 }
 
 class AuctionsBind extends Component<AuctionsProps> {
@@ -79,9 +124,196 @@ class AuctionsBind extends Component<AuctionsProps> {
             return false;
         }
     }
+    filterAuctions() {
+        let activeAuctions = [...this.props.auctions.activeAuctions];
+        let filters = {...this.state.filters};
+
+        let union:Array<IAuctionItem> = [];
+        let atLeastOneFilter = false;
+        if(filters.myBids) {
+            union = [...union, ...activeAuctions.filter((a:IAuctionItem) => {
+                return a.bidderid === this.props.userinfo.id
+            })];
+            atLeastOneFilter = true;
+        }
+        if(filters.mySales) {
+            union = [...union, ...activeAuctions.filter((a:IAuctionItem) => {
+                return a.sellerid === this.props.userinfo.id
+            })]
+            atLeastOneFilter = true;
+        }
+        if(filters.watching) {
+            this.props.auctions.subscriptions.forEach((s:string) => {
+                activeAuctions.forEach((a:IAuctionItem) => {
+                    if(a.auctionID === s) {
+                        union.push({...a})
+                    }
+                })
+            })
+            atLeastOneFilter = true;
+        }
+        if(filters.hasBid) {
+            union = [...union, ...activeAuctions.filter((a:IAuctionItem) => {
+                return a.bidder !== null
+            })]
+            atLeastOneFilter = true;
+        }
+        if(filters.noBidders) {
+            union = [...union, ...activeAuctions.filter((a:IAuctionItem) => {
+                return a.bidder === null
+            })]
+            atLeastOneFilter = true;
+        }
+        if(!atLeastOneFilter) {
+            union = [...activeAuctions];
+        }
+        if(filters.item !== null) {
+            union = union.filter((a:IAuctionItem) => {
+                return a.item === filters.item
+            })
+        }
+
+        let auctionHash:{[key:string]:IAuctionItem} = {};
+        for(let j = 0; j < union.length; j++) {
+            auctionHash[union[j].auctionID] = {...union[j]}
+        }
+        let auctionSet:Array<IAuctionItem> = [];
+        Object.keys(auctionHash).forEach((auctionID:string) => {
+            auctionSet.push(auctionHash[auctionID]);
+        })
+
+        if(filters.sortMethod === AuctionSort.Bidder) {
+            auctionSet.sort((a:IAuctionItem, b:IAuctionItem) => {
+                if(a.bidder === null) {
+                    return -1;
+                }
+                if(b.bidder === null) {
+                    return 1;
+                }
+                return ('' + a.bidder).localeCompare(b.bidder);
+            })
+        }
+        if(filters.sortMethod === AuctionSort.Seller) {
+            auctionSet.sort((a:IAuctionItem, b:IAuctionItem) => {
+                return ('' + a.seller).localeCompare(b.seller);
+            })
+        }
+        if(filters.sortMethod === AuctionSort.ExpirationTime) {
+            auctionSet.sort((a:IAuctionItem, b:IAuctionItem) => {
+                return a.expiration - b.expiration;
+            })
+        }
+        if(filters.sortMethod === AuctionSort.Price) {
+            auctionSet.sort((a:IAuctionItem, b:IAuctionItem) => {
+                return a.currentBid - b.currentBid;
+            })
+        }
+        if(filters.sortMethod === AuctionSort.Item) {
+            auctionSet.sort((a:IAuctionItem, b:IAuctionItem) => {
+                return ('' + a.item).localeCompare(b.item);
+            })
+        }
+        return auctionSet;
+    }
+
+    setFilterToggle(property:any) {
+        this.setState({
+            filters: {
+                ...this.state.filters,
+                ...property
+            }
+        })
+    }
+
+    setSortMethod(e:string) {
+
+        let sortMethod:AuctionSort = AuctionSort.ExpirationTime;
+        if(e === 'Price') {
+            sortMethod = AuctionSort.Price;
+        } else if(e === 'Expiration') {
+            sortMethod = AuctionSort.ExpirationTime;
+        } else if(e === 'Item') {
+            sortMethod = AuctionSort.Item;
+        } else if(e === 'Seller') {
+            sortMethod = AuctionSort.Seller;
+        } else if(e === 'Bidder') {
+            sortMethod = AuctionSort.Bidder;
+        }
+        this.setState({
+            ...this.state,
+            filters: {
+                ...this.state.filters,
+                sortMethod
+            }
+        })
+    }
+
+    checkBoxClass(property:keyof AuctionFilters) {
+        let cn = "checkbox ";
+        if(this.state.filters[property]) {
+            cn += "active";
+        }
+        return cn;
+    }
+
+    renderItemSelectItem(item:string|null) {
+        if(item) {
+            if(!this.props.itemcatalogue[item].tradeable) return null;
+        }
+        return(
+            <div 
+                className="item-item"
+                onClick={() => this.setState({
+                    ...this.state,
+                    filters: {
+                        ...this.state.filters,
+                        item
+                    },
+                    showItemSelect:false
+                })}>
+                {
+                    item? 
+                    this.props.itemcatalogue[item].name
+                    : "All"
+                }
+            </div>
+        )
+    }
+
+    watchAll() {
+        let subscriptions:Array<string> = this.props.auctions.activeAuctions.map((a:IAuctionItem) => {return a.auctionID});
+        this.props.setAuctionSubscriptions(subscriptions);
+        if(storageAvailable()) {
+            localStorage.setItem('nasfaq:auction_subscriptions', JSON.stringify(subscriptions));
+        }
+    }
+
+    watchAllMine() {
+        let subscriptions:Array<string> = [];
+        this.props.auctions.activeAuctions.forEach((a:IAuctionItem) => {
+            if(a.sellerid === this.props.userinfo.id) {
+                subscriptions.push(a.auctionID);
+            }
+        });
+        this.props.setAuctionSubscriptions(subscriptions);
+        if(storageAvailable()) {
+            localStorage.setItem('nasfaq:auction_subscriptions', JSON.stringify(subscriptions));
+        }
+    }
+
+    unwatchAll() {
+        this.props.setAuctionSubscriptions([]);
+        if(storageAvailable()) {
+            localStorage.setItem('nasfaq:auction_subscriptions', JSON.stringify([]))
+        }
+    }
+
     render() {
         if(!this.props.userinfo.loaded) return null;
         if(!this.props.session.loggedin) return <Redirect to="/login/auctions"/>;
+
+        let auctions:Array<IAuctionItem> = this.filterAuctions();
+
         return(
             <div className="container fill">
                 {
@@ -107,20 +339,25 @@ class AuctionsBind extends Component<AuctionsProps> {
                                     Past Auctions
                             </div>
                         </div>
-                        { 
-                            this.hasLicense() ?
+                        <div className="left-action-container">
+                            { 
+                                this.hasLicense() ?
+                                <div 
+                                    className="add-auction-button"
+                                    onClick={() => this.setState({showCreateWindow: !this.state.showCreateWindow})}>
+                                    Create Auction
+                                </div>
+                                :
+                                <div 
+                                    className="buy-auction-license-button"
+                                    onClick={() => this.setState({showBuyLicenseWindow: !this.state.showBuyLicenseWindow})}>
+                                    Buy License
+                                </div>
+                            }
                             <div 
-                                className="add-auction-button"
-                                onClick={() => this.setState({showCreateWindow: !this.state.showCreateWindow})}>
-                                Create Auction
-                            </div>
-                            :
-                            <div 
-                                className="buy-auction-license-button"
-                                onClick={() => this.setState({showBuyLicenseWindow: !this.state.showBuyLicenseWindow})}>
-                                Buy License
-                            </div>
-                        }
+                                className="toggle-filters-button"
+                                onClick={() => this.setState({showFilters: !this.state.showFilters})}>{this.state.showFilters ? "Hide " : "Show "} Filters</div>
+                        </div>
                         <div 
                             className="info-button"
                             onClick={() => this.setState({showInfoWindow: !this.state.showInfoWindow})}>
@@ -133,27 +370,117 @@ class AuctionsBind extends Component<AuctionsProps> {
                     { this.state.showBuyLicenseWindow && !this.hasLicense() ?
                         <BuyLicense /> : null
                     }
-                    <div className="auction-container">
+                    
                     {
-                        this.state.activeView === AuctionView.LIVE ?
-                        <>
+                        this.state.showItemSelect && this.state.showFilters ?
+                        <div className="item-select-container"> 
+                            {this.renderItemSelectItem(null)}
                             {
-                                this.props.auctions.activeAuctions.map((item:IAuctionItem) => 
-                                <AuctionPreview
-                                    key={item.auctionID}
-                                    auction={item}/>
+                                Object.keys(this.props.itemcatalogue).map((item:string) => 
+                                    this.renderItemSelectItem(item)
                                 )
-                            }
-                            { 
-                                this.props.auctions.activeAuctions.length === 0 ?
-                                <div className="no-active-auctions">
-                                    No Active Auctions!
-                                </div> : null
-                            }
-                        </>
-                        :
-                        <AuctionHistory/>
+                            }               
+                        </div> : null
                     }
+
+                    <div className="auction-content-outer">
+                        {
+                            this.state.showFilters ?
+                            <div className="auction-filter-controls">
+
+
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">My Bids</div>
+                                    <div 
+                                        className={this.checkBoxClass("myBids")}
+                                        onClick={() => this.setFilterToggle({myBids:!this.state.filters.myBids})}></div>
+                                </div>
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">My Sales</div>
+                                    <div 
+                                        className={this.checkBoxClass("mySales")}
+                                        onClick={() => this.setFilterToggle({mySales:!this.state.filters.mySales})}></div>
+                                </div>
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">Watching</div>
+                                    <div 
+                                        className={this.checkBoxClass("watching")}
+                                        onClick={() => this.setFilterToggle({watching:!this.state.filters.watching})}></div>
+                                </div>
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">Item</div>
+                                    <div 
+                                        className="item-select"
+                                        onClick={() => this.setState({
+                                            showItemSelect:!this.state.showItemSelect
+                                        })}>
+                                        {
+                                        this.state.filters.item ?
+                                        this.state.filters.item : "None"}
+                                    </div>
+                                </div>
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">Has Bid</div>
+                                    <div 
+                                        className={this.checkBoxClass("hasBid")}
+                                        onClick={() => this.setFilterToggle({hasBid:!this.state.filters.hasBid})}></div>
+                                </div>
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">No Bidders</div>
+                                    <div 
+                                        className={this.checkBoxClass("noBidders")}
+                                        onClick={() => this.setFilterToggle({noBidders:!this.state.filters.noBidders})}></div>
+                                </div>
+                                <div className="auction-filter-control-row">
+                                    <div className="filter-label">Sort by</div>
+                                    <DropdownInput 
+                                        label=""
+                                        options={["Price", "Expiration", "Item", "Seller", "Bidder"]}
+                                        default={"Expiration"}
+                                        onChange={(e) => {this.setSortMethod(e)}}
+                                        />
+                                </div>
+                                <div className="auction-filter-control-row center">
+                                    <div 
+                                        className="auction-control-button"
+                                        onClick={() => this.watchAll()}>
+                                        Watch All
+                                    </div>
+                                    <div 
+                                        className="auction-control-button"
+                                        onClick={() => this.unwatchAll()}>
+                                        Unwatch All
+                                    </div>
+                                    <div 
+                                        className="auction-control-button"
+                                        onClick={() => this.watchAllMine()}>
+                                        Watch All Mine
+                                    </div>
+                                </div>
+                            </div> : null
+                        }
+                        <div className="auction-container">
+                        {
+                            this.state.activeView === AuctionView.LIVE ?
+                            <>
+                                {
+                                    auctions.map((item:IAuctionItem) => 
+                                    <AuctionPreview
+                                        key={item.auctionID}
+                                        auction={item}/>
+                                    )
+                                }
+                                { 
+                                    auctions.length === 0 ?
+                                    <div className="no-active-auctions">
+                                        No Active Auctions!
+                                    </div> : null
+                                }
+                            </>
+                            :
+                            <AuctionHistory/>
+                        }
+                        </div>
                     </div>
                 </div>
             </div>
@@ -161,6 +488,9 @@ class AuctionsBind extends Component<AuctionsProps> {
     }
 }
 
-const Auctions = connect(mapStateToProps)(AuctionsBind);
+const Auctions = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AuctionsBind);
 
 export default Auctions;
